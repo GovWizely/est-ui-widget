ffbd6038b4394f7481da533cfe458b55fc9868cb
Calum Halcrow <calum@rrsoft.co>
2015-11-05 13:03:13 +0800

    USEAC title tweak

--- javascript/widget.js
+++ javascript/widget.js
@@ -145,7 +145,7 @@
             includeCountries: true
           },
           useac_locations: {
-            title: 'Export Assistance Center Locations',
+            title: 'Export Assistance Centers',
             resultTitleField: function (val) { return val['zip_code'] + ' - ' + val['office_name']; },
             displayFields: ['zip_code', 'office_name', WidgetFieldFormatter.format_array('address'), 'email', 'phone'],
             extraParams: {},
